3bc7458323a71bc8010d230d4d473f965870eaa9
SergeyScherbo <sergey31scherbo@gmail.com>
2017-09-22 00:24:25 +0400

    Add files via upload

---
+++ contactForm/js/app.js
@@ -0,0 +1,38 @@
+$(function() {
+    var form = $('#ajax-contact');
+    var formMessage = $('.form-message');
+
+    $(form).submit(function(event) {
+        event.preventDefault();
+
+        var formData = $(form).serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessage).removeClass('error');
+            $(formMessage).addClass('success');
+
+            $(formMessage).text(response);
+
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        })
+        .fail(function(response) {
+            $(formMessage).removeClass('success');
+            $(formMessage).addClass('error');
+
+            if (data.responseText !== '') {
+                $(formMessage).text(data.responseText);
+            } else {
+                $(formMessage).text('Oops! An error occured and your message couldn\'t be sent.');
+            }
+        })
+
+
+    });
+});
